2052e9c7465cb64628ab90f76f3da340a55d8370
Andrea <a.posi@procurementleaders.com>
2023-09-29 02:29:05 +0100

    Genesis

---
+++ README.md
@@ -0,0 +1,9 @@
+# ke
+
+## RAKE
+Rapid Automatic Keyword Extraction
+Contributions from https://github.com/u-prashant/RAKE
+
+## YAKE
+Yet Another Keyword Extractor
+Contributions from https://github.com/LIAAD/yake
---
+++ src/test.py
@@ -0,0 +1,50 @@
+from yake import yake
+from rake import rake
+import pprint
+import os
+import time
+
+## Setup
+pp = pprint.PrettyPrinter(indent=4)
+script_dir = os.path.dirname(__file__)
+stoppath = os.path.join(script_dir, "stoplist/it.txt")
+
+documents = ["micromega", "valore_tempo"]
+documents_paths = [
+    os.path.join(script_dir, "documents/" + doc + ".txt") for doc in documents
+]
+
+rake_object = rake.Rake(stoppath, 5, 3, 4)
+kwe = yake.KeywordExtractor(stop_file=stoppath)
+
+
+def average(lst):
+    return sum(lst) / len(lst)
+
+
+def test(ke):
+    for document in documents_paths:
+        sample_file = open(document, "r", encoding="utf-8")
+        text = sample_file.read()
+        times = []
+        start_time = time.time()
+        for _ in range(1, 21):
+            rake_object.run(text)
+            times.append(time.time() - start_time)
+        times.sort()
+        print("###")
+        print("Document", os.path.basename(document))
+        print("Worst: ", times[-1])
+        print("Best: ", times[0])
+        print("Average: ", average(times[1:-1]))
+
+
+if __name__ == "__main__":
+    print("-----")
+    print("RAKE")
+    print("-----")
+    test(ke=rake_object.run)
+    print("-----")
+    print("YAKE")
+    print("-----")
+    test(ke=kwe.extract_keywords)
