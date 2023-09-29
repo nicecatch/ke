0e7782387f1dec980f1298dbc003bb753d956287
Andrea <a.posi@procurementleaders.com>
2023-09-29 02:53:25 +0100

    Printers

--- src/test.py
+++ src/test.py
@@ -14,6 +14,7 @@ documents_paths = [
     os.path.join(script_dir, "documents/" + doc + ".txt") for doc in documents
 ]
 
+
 rake_object = rake.Rake(stoppath, 5, 3, 4)
 kwe = yake.KeywordExtractor(stop_file=stoppath)
 
@@ -22,14 +23,15 @@ def average(lst):
     return sum(lst) / len(lst)
 
 
-def test(ke):
+def test(ke, printer):
     for document in documents_paths:
         sample_file = open(document, "r", encoding="utf-8")
         text = sample_file.read()
         times = []
         start_time = time.time()
+        keywords = []
         for _ in range(1, 21):
-            rake_object.run(text)
+            keywords = ke(text)
             times.append(time.time() - start_time)
         times.sort()
         print("###")
@@ -37,14 +39,19 @@ def test(ke):
         print("Worst: ", times[-1])
         print("Best: ", times[0])
         print("Average: ", average(times[1:-1]))
+        # print('Keywords: ')
+        # printer(keywords)
 
+def print_k(keywords):
+    for kw in keywords:
+        print(kw)
 
 if __name__ == "__main__":
     print("-----")
     print("RAKE")
     print("-----")
-    test(ke=rake_object.run)
+    test(ke=rake_object.run, printer=print_k)
     print("-----")
     print("YAKE")
     print("-----")
-    test(ke=kwe.extract_keywords)
+    test(ke=kwe.extract_keywords, printer=print_k)
